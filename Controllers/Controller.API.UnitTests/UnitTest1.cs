24ea8c72765c48b44bbe75f564119b8da2f6dc49
Scott Crowther <scottcrowther@hotmail.com>
2022-08-05 09:03:39 +0100

    Basic Structure of known patterns

---
+++ Controllers/Controller.API.UnitTests/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace Controller.API.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
---
+++ Minimal-MediatR/MinimalMediatR.Api.UnitTests/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace MinimalMediatR.Api.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
